7339d3e38189680c58eefd1bf9b9812d7c6bcb29
victorfernandesraton <vfbrato@gmail.com>
2022-11-22 22:49:37 -0300

    remove error from directory is not exist

--- icon/file_test.go
+++ icon/file_test.go
@@ -21,15 +21,6 @@ func TestFindIconsByDir(t *testing.T) {
 			t.Fatalf("not valid size expected %vx%v, got %vx%v", data[0].Size.Height, data[0].Size.Width, 32, 32)
 		}
 	})
-	t.Run("error when directory does not exist", func(t *testing.T) {
-		data, err := icon.ParseIconListFromDir("../ok")
-		if data != nil {
-			t.Fatalf("expected nil data, got %v", data)
-		}
-		if err.Error() != "open ../ok: no such file or directory" {
-			t.Fatalf("expected open ../ok: no such file or directory, got %v", err)
-		}
-	})
 	t.Run("empty list of icons", func(t *testing.T) {
 		data, err := icon.ParseIconListFromDir("../testdata/empty")
 		if data != nil {
